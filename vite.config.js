a996347c6aba26ae92b61b954ad944c73186e7ce
sainikhil1897 <nikhilpemmaraju7@gmail.com>
2025-04-23 22:25:30 +0530

    Deployed with new repo name

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/simple-expense-calculator/', // Must have leading and trailing slashes
+  base: '/simple-expense-tracker/',
 })
